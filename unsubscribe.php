c56cafa504a1128be49a938f1c42480509aec3e1
root <seanw8429@gmail.com>
2022-10-10 20:51:52 +0000

    Unsubscribe

--- unsubscribe.php
+++
@@ -1,9 +0,0 @@
-<?php
-$pagetitle = "Unsubscribe";
-$to = "support@platinumhost.xyz";
-$subject = "Unsubscribed";
-$message = "Someone has unsubscribed from the mailing list";
-$headers = "From: Platinum Hosting <";
-mail($to,$subject,$message,$headers,"-f".$to);
-echo "You have been unsubscribed from the mailing list";
-?>

318879a4bed17b6ccf4ed357b4b720e665ce371c
root <seanw8429@gmail.com>
2022-11-01 18:32:56 +0000

    Removed js files

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-"use strict";
+// "use strict";
 
-async function myFunction(element) {
-    var copyText = document.getElementById("coupon-button");
-    if (copyText.innerHTML === "<strong>LA45OFF</strong>") {
-        var $temp = $("<input>");
-        $("body").append($temp);
-        $temp.val($(element).text()).select();
-        document.execCommand("copy");
-        $temp.remove();
-        copyText.innerHTML = "<strong>Copied to clipboard</strong>";
-        await sleep(1500);
-        copyText.innerHTML = "<strong>LA45OFF</strong>";
-    } else {
-        copyText.innerHTML = "<strong>LA45OFF</strong>";
-    }
-}
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+// async function myFunction(element) {
+//     var copyText = document.getElementById("coupon-button");
+//     if (copyText.innerHTML === "<strong>LA45OFF</strong>") {
+//         var $temp = $("<input>");
+//         $("body").append($temp);
+//         $temp.val($(element).text()).select();
+//         document.execCommand("copy");
+//         $temp.remove();
+//         copyText.innerHTML = "<strong>Copied to clipboard</strong>";
+//         await sleep(1500);
+//         copyText.innerHTML = "<strong>LA45OFF</strong>";
+//     } else {
+//         copyText.innerHTML = "<strong>LA45OFF</strong>";
+//     }
+// }
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
